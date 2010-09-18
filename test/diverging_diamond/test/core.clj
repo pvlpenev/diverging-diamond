59c10ae442a8146d1e1af43702a2c77ab380b48a
pav <pav@pav.(none)>
2010-09-18 21:54:10 +0300

    first commit

---
+++ test/diverging_diamond/test/core.clj
@@ -0,0 +1,6 @@
+(ns diverging-diamond.test.core
+  (:use [diverging-diamond.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
