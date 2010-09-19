bba9055bd50b2a7bfb1f7ff82c3482692b68d071
Pavel Penev <pavelludiq@gmail.com>
2010-09-19 16:53:24 +0300

    added home page

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [sqlitejdbc "0.5.6"]
                  [compojure "0.4.1"]
+                 [ring/ring-jetty-adapter "0.2.5"]
                  [sandbar/sandbar "0.3.0-SNAPSHOT"]
-                 [jbcrypt "0.3"]]
+                 [jbcrypt "0.3"]
+                 [hiccup "0.2.7"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
 
--- src/diverging_diamond/core.clj
+++ src/diverging_diamond/core.clj
@@ -1,2 +1,22 @@
 (ns diverging-diamond.core
-  (:require [diverging-diamond.db-layer :as db]))
+  (:use [diverging-diamond.db-layer :as db]
+	[diverging-diamond.html :as html]
+	[ring.adapter.jetty :only [run-jetty]]
+        [compojure.core])
+  (:require [compojure.route :as route]))
+
+(defn home []
+  (let [links (db/get-links)]
+    (layout "Home"
+	    (for [link links]
+	       [:div (:id link) [:a {:href (:url link)} (:title link)]]))))
+
+(defroutes ddroutes
+  (GET "/" [] (home))
+  (route/files "/")
+  (route/not-found "<h1>Not Found</h1>"))
+
+
+(defn start []
+  (run-jetty (var ddroutes) {:port 8080
+                           :join? false}))
